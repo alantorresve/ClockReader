2157a5bbc6ee7be7cc77484c0a13206e5c1360de
alantorresve <alantorresve@gmail.com>
2023-10-03 22:10:33 +0800

    just need to fix the hour detection now

--- libraries/image_processor.py
+++ libraries/image_processor.py
@@ -1,30 +1,26 @@
 import cv2
 import numpy as np
 
-def resize_image(src, width = 600):
+def resize_image(src, width=600):
     # Calculate the aspect ratio and resize the image
     height = int(src.shape[0] * (width / src.shape[1]))
     resized_image = cv2.resize(src, (width, height))
     return resized_image
 
-
 def load_image(filename):
     return cv2.imread(filename, cv2.IMREAD_COLOR)
-
 
 def to_gray(image):
     return cv2.medianBlur(
         cv2.cvtColor(image, cv2.COLOR_BGR2GRAY), 5
     )
 
-
 def process_circles(gray_image):
     return cv2.HoughCircles(gray_image, cv2.HOUGH_GRADIENT, dp=1,
-                               minDist=gray_image.shape[0] / 1,
-                               param1=100, param2=30, minRadius=200, maxRadius=500)
+                            minDist=gray_image.shape[0] / 1,
+                            param1=100, param2=30, minRadius=200, maxRadius=500)
 
-
-def detect_line(circle, source_image, gray_image):
+def detect_line(circle, source_image, gray_image, filtered_lines):
     center = (circle[0], circle[1])
     radius = circle[2]
     # Draw circle center
@@ -36,49 +32,50 @@ def detect_line(circle, source_image, gray_image):
     edges = cv2.Canny(gray_image, 50, 150)
     lines = cv2.HoughLinesP(edges, 1, np.pi / 180, threshold=100, minLineLength=60, maxLineGap=5)
 
-    filtered_lines = []  # Initialize an empty list to store filtered lines
+    line_angle_array = []  # Initialize an empty list to store all lines with angles
 
     if lines is not None:
         for line in lines:
+            found = False
             x1, y1, x2, y2 = line[0]
             if abs(y1 - center[1]) < 20 or abs(y2 - center[1]) < 20:
-                cv2.line(source_image, (x1, y1), (x2, y2), (0, 255, 0), 2)
-
                 angle = np.arctan2(y2 - y1, x2 - x1) * 180 / np.pi  # Calculate the angle of the line
-                found = False
-
-                # Check if the current line is similar to any existing lines
-                for existing_line in filtered_lines:
-                    if abs(angle - existing_line['angle']) < 10:
+                line_with_angle = np.append(line[0], angle)  # Add the angle to the line
+                line_angle_array.append(line_with_angle)  # Add the line to the list
+            
+            if len(filtered_lines) > 0:
+                for existing_line in filtered_lines:  # Iterate over existing lines
+                    if abs(angle - existing_line[-1]) < 10:  # Compare the angle (last element)
                         # If the angle is similar, compare and keep the longer line
                         length = np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
-                        existing_length = np.sqrt(
-                            (existing_line['x2'] - existing_line['x1']) ** 2 +
-                            (existing_line['y2'] - existing_line['y1']) ** 2
+                        length_ = np.sqrt(
+                            (existing_line[2] - existing_line[0]) ** 2 +
+                            (existing_line[3] - existing_line[1]) ** 2
                         )
-                        if length > existing_length:
+                        if length_ > length:
                             # Update the existing line
-                            existing_line['x1'], existing_line['y1'] = x1, y1
-                            existing_line['x2'], existing_line['y2'] = x2, y2
-                            existing_line['angle'] = angle
+                            existing_line[0], existing_line[1] = x1, y1
+                            existing_line[2], existing_line[3] = x2, y2
+                            existing_line[-1] = angle
                         found = True
                         break
 
-                # If no similar line was found, add the current line as a new filtered line
-                if not found:
-                    filtered_lines.append({
-                        'x1': x1,
-                        'y1': y1,
-                        'x2': x2,
-                        'y2': y2,
-                        'angle': angle
-                    })
+            # If no similar line was found, add the current line as a new filtered line
+            if not found:
+                filtered_lines.append([x1, y1, x2, y2, angle])
 
+    for line in filtered_lines:
+        x1, y1, x2, y2, angle = line
+        cv2.line(source_image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+    # print(line_angle_array)
+    # print(filtered_lines)
     return source_image
+
 
 def detect_clock_hands(filtered_lines):
     # Sort the lines by length from longest to shortest
-    sorted_lines = sorted(filtered_lines, key=lambda x: np.sqrt((x['x2'] - x['x1'])**2 + (x['y2'] - x['y1'])**2), reverse=True)
+    sorted_lines = sorted(filtered_lines, key=lambda x: np.sqrt((x[2] - x[0])**2 + (x[3] - x[1])**2), reverse=True)
 
     clock_hands = {
         'hour': None,
@@ -89,11 +86,11 @@ def detect_clock_hands(filtered_lines):
     # Take the three longest lines (seconds, minutes, and hour)
     for i, line in enumerate(sorted_lines[:3]):
         if i == 0:
-            clock_hands['seconds'] = line
+            clock_hands['seconds'] = {'x1': line[0], 'y1': line[1], 'x2': line[2], 'y2': line[3], 'angle': line[4]}
         elif i == 1:
-            clock_hands['minutes'] = line
+            clock_hands['minutes'] = {'x1': line[0], 'y1': line[1], 'x2': line[2], 'y2': line[3], 'angle': line[4]}
         elif i == 2:
-            clock_hands['hour'] = line
+            clock_hands['hour'] = {'x1': line[0], 'y1': line[1], 'x2': line[2], 'y2': line[3], 'angle': line[4]}
 
     return clock_hands
 
@@ -124,3 +121,5 @@ def detect_exact_time(clock_hands):
 
     else:
         return "Clock hands not detected"
+
+
